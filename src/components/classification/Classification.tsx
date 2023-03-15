6f265c5b6cf7bdd11a5b5346462b59a322de9d77
Nicolas Pope <npope@uef.fi>
2023-03-15 07:20:59 +0000

    Merged PR 21: #13 Add example upload and drop
    Add example upload and drop
    
    Related work items: #13

--- src/components/classification/Classification.tsx
+++ src/components/classification/Classification.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import style from "./classification.module.css";
 import { IClassification } from "../../state";
 import { Button } from "../button/Button";
@@ -6,8 +6,10 @@ import { Widget } from "../widget/Widget";
 import Sample from "./Sample";
 import WebcamCapture from "./WebcamCapture";
 import VideocamIcon from '@mui/icons-material/Videocam';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
 import ClassMenu from "./ClassMenu";
 import { useTranslation } from 'react-i18next';
+import { useDropzone } from "react-dropzone";
 
 interface Props {
     name: string;
@@ -21,6 +23,46 @@ interface Props {
 
 export function Classification({name, active, data, setData, onActivate, setActive, onDelete}: Props) {
     const {t} = useTranslation();
+
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        const promises = acceptedFiles.map((file) => new Promise<HTMLCanvasElement>((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onabort = () => reject();
+            reader.onerror = () => reject();
+            reader.onload= () => {
+                const newCanvas = document.createElement("canvas");
+                newCanvas.width = 224;
+                newCanvas.height = 224;
+                newCanvas.style.width = "58px";
+                const ctx = newCanvas.getContext("2d");
+                const img = new Image();
+                img.onload = () => {
+                    ctx?.drawImage(img, 0, 0, 224, 224);
+                    resolve(newCanvas);
+                }
+                img.src = reader.result as string;
+            }
+            reader.readAsDataURL(file);
+        }));
+
+        Promise.all(promises).then((results: HTMLCanvasElement[]) => {
+            setData({
+                label: data.label,
+                samples: [...data.samples, ...results],
+            });
+        });
+    }, [setData, data]);
+
+    const {getRootProps, getInputProps, isDragActive, open} = useDropzone({
+        noClick: true,
+        onDrop,
+        accept: {
+            "image/png": [".png"],
+            "image/jpeg": [".jpg", ".jpeg"],
+            "image/gif": [".gif"],
+        },
+        maxFiles: 30,
+    });
 
     return <Widget title={name} setTitle={(title: string) => {
         setData({
@@ -39,7 +81,8 @@ export function Classification({name, active, data, setData, onActivate, setActi
                     samples: [...data.samples, image],
                 });
             }} onClose={() => setActive(false)}/> : null}
-            <div className={style.listContainer}>
+            <div className={style.listContainer} {...getRootProps()}>
+                <input {...getInputProps()} />
                 {data.samples.length === 0 && <div className={style.samplesLabel}>
                     {t("trainingdata.labels.addSamples")}:
                 </div>}
@@ -48,8 +91,13 @@ export function Classification({name, active, data, setData, onActivate, setActi
                 </div>}
                 <ol className={(active) ? style.samplelistLarge : style.samplelistSmall}>
                     {!active && <li className={style.sample}>
-                        <Button sx={{"& .MuiButton-startIcon": { margin: "0px"}, flexDirection: "column"}} variant="outlined" startIcon={<VideocamIcon />} onClick={onActivate}>
+                        <Button variant="outlined" startIcon={<VideocamIcon />} onClick={onActivate}>
                             {t("trainingdata.actions.webcam")}
+                        </Button>
+                    </li>}
+                    {!active && <li className={style.sample}>
+                        <Button sx={{"& .MuiButton-startIcon": { margin: "0px"}, flexDirection: "column"}} variant="outlined" startIcon={<UploadFileIcon />} onClick={open}>
+                            {t("trainingdata.actions.upload")}
                         </Button>
                     </li>}
                     {data.samples.map((s, ix) => <Sample key={ix} image={s} onDelete={() => {
@@ -59,6 +107,7 @@ export function Classification({name, active, data, setData, onActivate, setActi
                         });
                     }} />)}
                 </ol>
+                {isDragActive && <div className={style.dropOverlay}>{t("trainingdata.labels.dropFiles")}</div>}
             </div>
         </div>
     </Widget>;
